7ca9831d0368bbd22378013d28bc5f24b013ceb3
Le Zhang <leeinsteinzhang@gmail.com>
2024-04-09 17:05:07 -0400

    fixed issue#1

--- edx/modules/edx_diff.py
+++ edx/modules/edx_diff.py
@@ -27,67 +27,42 @@ def get_student_list(classlist_file: str):
                 studentList.append(line.split(',')[1])
     return studentList
 
+
 def diff(old_result, new_result, classlist):
+    """
+    Compares scores between two result sets for assignments by name,
+    ensuring alignment in printed output.
+    """
     student_list = get_student_list(classlist)
     old_scores = {}
     new_scores = {}
+
     with open(old_result, mode='r') as file1:
         data1 = csv.reader(file1)
+        headers_old = next(data1)
         for row in data1:
-            old_scores[row[0]] = row[1:]
-    
+            student_scores = {headers_old[i]: row[i] for i in range(1, len(row))}
+            old_scores[row[0]] = student_scores
+
     with open(new_result, mode='r') as file2:
         data2 = csv.reader(file2)
+        headers_new = next(data2)
         for row in data2:
-            new_scores[row[0]] = row[1:]
+            student_scores = {headers_new[i]: row[i] for i in range(1, len(row))}
+            new_scores[row[0]] = student_scores
 
-    old_more = False
-    new_more = False
-    assements_old = old_scores['']
-    assements_new = new_scores['']
-    len_old = len(assements_old)
-    len_new = len(assements_new)
-    midterm_old = 'Midterm' in assements_old
-    midterm_new = 'Midterm' in assements_new
+    common_assignments = set(headers_old).intersection(headers_new)
+    common_assignments.remove('')
 
-    if len_old != len_new:
-        if len_old < len_new:
-            new_more = True
-            assements = assements_old
-        else:
-            old_more = True
-            assements = assements_new
-    else:
-        assements = assements_old
-    
     for uw_id in student_list:
-        if uw_id in old_scores and uw_id in new_scores:
-            old_data = old_scores[uw_id]
-            new_data = new_scores[uw_id]
-
-            if len_old < len_new:
-                if midterm_new:
-                    new_data = new_data[:len_old - 1] + new_data[-1:]
-                else:
-                    new_data = new_data[:len_old]
-            else:
-                if midterm_old:
-                    old_data = old_data[:len_new - 1] + old_data[-1:]
-                else:
-                    old_data = old_data[:len_new]
-            for i in range(len_old - 1):
-                if new_data[i] != old_data[i]:
-                    same = False
-                    print(f"[{uw_id} - {assements[i]}]: {old_data[i]} -> {new_data[i]}")
-        elif uw_id in old_scores and uw_id not in new_scores:
-            print(f"{uw_id} Not in new edx_marks.csv")
-        elif uw_id not in old_scores and uw_id in new_scores:
-            print(f"{uw_id} Not in old edx_marks.csv")
-    
-    if new_more:
-        print(f"New edx_marks.csv has more Assements")
-    if old_more:
-        print(f"Old edx_marks.csv has more Assements")
+        if uw_id not in old_scores or uw_id not in new_scores:
+            continue
+        
+        for assignment in common_assignments:
+            old_score = old_scores[uw_id].get(assignment, "Missing")
+            new_score = new_scores[uw_id].get(assignment, "Missing")
+            if old_score != new_score:
+                 print(f"[{uw_id:<10} - {assignment}] {old_score} -> {new_score}")
 
 
 def main():
